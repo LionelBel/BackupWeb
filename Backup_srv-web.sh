4244a9be3ee59354159371abf36cf9c98deae790
Lionel <lionel.bellerose@outlook.com>
2021-11-05 19:16:48 +0100

    Ajout de log à chaque étape de la sauvegarde

--- Backup_srv-web.sh
+++ Backup_srv-web.sh
@@ -35,6 +35,14 @@ echo " Répertoire à sauvegarder : $source1 "
 # Copie du répertoire WordPress
 echo " Copie du répertoire : $name, dans le répertoire sauvegarde local "
 cp -r $source1 $dest/$name-$date
+
+## Status de la copie du répertoire WordPress
+status=$?
+case $status in 
+	0) echo "Copie du répertoire wordpress effectuée" >> $log;;
+	1) echo "Erreur : copie du répertoire wordpress non effectuée" >> $log && exit;;
+esac
+
 # tar czvf $dest/$name-$date.tar.gz $dest
 
 # Envoi de l'archive sur le serveur ftp
@@ -53,11 +61,18 @@ cp -r $source1 $dest/$name-$date
 #########################################
 # Définition des variables
 mysql_user=root
-name_mysql="msql"
+name_mysql="mysql"
 
 # Sauvegarde de la base de donnée wordpress
 echo " Sauvegarde de la base de donnée : wordpress "
 mysqldump --user=$mysql_user --databases wordpress > $dest/$name_mysql-$date.sql
+
+## Status de la copie de la base de donnée WordPress
+status=$?
+case $status in 
+	0) echo "Copie de la base de donnée wordpress effectuée" >> $log;;
+	1) echo "Erreur : copie de la base de donnée wordpress non effectuée" >> $log && exit;;
+esac
 
 # Envoi de l'archive sur le serveur ftp
 # echo " Envoi des fichiers sur $hostname "
@@ -78,7 +93,7 @@ source2="/etc/apache2"
 source3="/etc/mysql"
 source4="/etc/php"
 name_conf_apache="apacheconf"
-name_conf_mysql="mysqlconf"
+name_conf_mysql="mysqlconf" 
 name_conf_php="phpconf"
 				#####################
 echo " Copie des fichiers de configuration Apache2 "
@@ -89,6 +104,13 @@ cp -r $source3 $dest/$name_conf_mysql-$date
 				######################
 echo " Copie des fichiers de configuration PHP "
 cp -r $source4 $dest/$name_conf_php-$date
+
+## Status de la copie des fichiers de configuration
+status=$?
+case $status in 
+	0) echo "Copie des fichiers de configuration effectuées" >> $log;;
+	1) echo "Erreur : copie des fichiers de configurations non effectuée" >> $log && exit;;
+esac
 
 #############################################
 ### Envoi des archives sur le serveur ftp ###
@@ -106,14 +128,27 @@ rm -rf $dest/$name_conf_php-$date
 rm -rf $dest/$name_mysql-$date.sql
 rm -rf $dest/$name-$date
 
-
 # Envoi de l'archive sur le serveur ftp
 echo " Envoi des fichiers sur $name_backup-$date.tar.gz " 
 lftp ftp://$user_ftp:$pass_ftp@$ftp_srv -e "mirror -R $dest/ /home/ftpuser/ftp_dir/sauvegarde/$date;quit"
 
+## Status de la création et transfert de l'archive
+status=$?
+case $status in 
+	0) echo "Création et transfert de l'archive effectuée" >> $log;;
+	1) echo "Erreur : création et transfert de l'archive non effectuée" >> $log && exit;;
+esac
+
 # Rotation des sauvegardes
-lftp ftp://$user_ftp:$pass_ftp@$ftp_srv -e "rm -rf $rotation;quit"
+lftp ftp://$user_ftp:$pass_ftp@$ftp_srv -e "rm -rf /home/ftpuser/ftp_dir/sauvegarde/$rotation;quit"
+
+## Status de la rotation
+status=$?
+case $status in 
+	0) echo "Rotation effectuée" >> $log;;
+	1) echo "Erreur : rotation non effectuée" >> $log && exit;;
+esac
 
 # Suppression du répertoire de sauvegarde local
 rm -fr $dest
-echo " Sauvegarde du serveur Web terminé "
+echo " Sauvegarde du serveur Web terminé " > $log
